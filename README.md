89d088e8cc53ecc4033628600802d776f3fbe840
Regis-tan <144893732+Regis-tan@users.noreply.github.com>
2025-03-17 18:30:01 +0700

    new fix

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+# Repository Description
+### This repository contains the answers to the WADS **(Web Application Development and Security)** Forum tasks for Week 6. It is a To-Do list Web Application with **Firebase/Firestore** Integration
 
+# Usage Instructions
+### Here are the instructions to use/deploy this Web Application:
+
+### 1. Clone this repositor on your IDE or Text editor using the command "git clone".
+### 2. Run "npm install" to install NodeJs Packages.
+### 3. Create a collection in Firestore called "tasks"
+### 4.
+### 5. Finally, to run the app locally, run "npm start".
--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import {initializeApp} from "firebase/app"
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyANfFrfMOlV4UzTDoy9Gjvao_NWtS_k4Pk",
-    authDomain: "wads-test.firebaseapp.com",
-    projectId: "wads-test",
-    storageBucket: "wads-test.firebasestorage.app",
-    messagingSenderId: "273950188057",
-    appId: "1:273950188057:web:ce9614e218e28f1d5d4088",
-    measurementId: "G-21QKVCBL1E"
+    apiKey: "(PLACEHOLDER)",
+    authDomain: "(PLACEHOLDER)",
+    projectId: "(PLACEHOLDER)",
+    storageBucket: "(PLACEHOLDER)",
+    messagingSenderId: "(PLACEHOLDER)",
+    appId: "(PLACEHOLDER)",
+    measurementId: "(PLACEHOLDER)"
   };
 
   const app = initializeApp(firebaseConfig)
